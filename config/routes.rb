faf39d2f5848be4414b6abc352c1e8fc737c667b
Maximilian Gerbaulet <maximilian@gerbaulethh.de>
2018-08-10 14:57:32 +0200

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-root to: "cocktails#index"
+root to: "coccktail#index"
 
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :ingredients, only: [:create]
